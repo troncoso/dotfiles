a4c875d5f24dbf74a93d4fa99c8cfde96f81e9ca
Noah Troncoso <noah.troncoso@gmail.com>
2023-02-16 15:04:01 -0500

    Adjust find file

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -3,5 +3,5 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
 
-vim.keymap.set("n", "<leader>_", ":NvimTreeFocus<CR>")
 vim.keymap.set("n", "<leader>-", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>_", ":NvimTreeFindFile<CR>")
